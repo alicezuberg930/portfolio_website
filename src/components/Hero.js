4675a0fe5da4e48032e714c0aa3fe97322dc6339
alicezuberg930 <tien23851@gmail.com>
2026-08-01 20:36:08 +0700

    remove img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -50,9 +50,9 @@ const Hero = memo(() => {
                 </div>
                 <div className="w-full lg:w-1/3">
                     <div className="aspect-[3/4] rounded-xl overflow-hidden">
-                        <motion.img src='/assets/about_3.jpg' alt="profile" className="object-cover w-full h-full"
+                        {/* <motion.img src='/assets/about_3.jpg' alt="profile" className="object-cover w-full h-full"
                             variants={slideRight({ delay: 0.6 })} whileInView="visible" initial="invisible"
-                        />
+                        /> */}
                     </div>
                 </div>
             </div>
